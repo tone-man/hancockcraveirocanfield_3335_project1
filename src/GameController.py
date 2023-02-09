c7ca2fff8173de8a76a7f6a487bb62e7a702c697
Toni <craveiroa@merrimack.edu>
2023-02-08 20:42:47 -0500

    Quick Comment Edit

--- src/GameController.py
+++ src/GameController.py
@@ -3,64 +3,82 @@ class GameController:
 
     def __init__(self, m, v) -> None:
         """Constructs a controller for the freeCell game.
-        m - model, in this case the Board
-        v - view, in this case the GameView
+
+        Keyword arguments:
+        m -- model, in this case the Board
+        v -- view, in this case the GameView
         """
         self.m = m
         self.v = v
 
     def moveCard(self, st, dt) -> None:
         """Moves a card from the tableau to another tableau
-        st - source tableau
-        dt - destination tebleau
+
+        Keyword arguments:
+        st -- source tableau
+        dt -- destination tebleau
         """
         pass
 
     def moveCard(self, st, dfc) -> None:
         """Moves a card from the tableau to a freecell.
-        st - source tableau
-        dfc - destination freecell
+
+        Keyword arguments:
+        st -- source tableau
+        dfc -- destination freecell
         """
         pass
 
     def moveCard(self, sfc, dt) -> None:
         """Moves a card from a freecell to a tableau
-        sfc - source free cell
-        dt - destination tableau
+
+        Keyword arguments:
+        sfc -- source free cell
+        dt -- destination tableau
         """
         pass
 
     def moveFoundation(self, t) -> None:
         """Moves a card from a tableau to a foundation pile
-        t - source tableau
+
+        Keyword arguments:
+        t -- source tableau
         """
         pass
 
     def moveFoundation(fc) -> None:
         """Moves a card from a freecell to a foundation
-        fc - source free cell
+
+        Keyword arguments:
+        fc -- source free cell
         """
         pass
 
     def isValidMove(self, c, dt) -> bool:
         """Checks that card placement is valid.
-        c - card in question
-        dt - destination tableau
+
+        Keyword arguments:
+        c -- card in question
+        dt -- destination tableau
         """
         return False
     
     def isValidMove(self, c, fc) -> bool:
         """Checks that card placement is valid.
-        c - card in question
-        fc - destination freecell
+
+        Keyword arguments:
+        c -- card in question
+        fc -- destination freecell
         """
         return False
 
     
     def isValidMove(self, c, dfp) -> bool:
         """Checks that card placement is valid
-        c - card in question
-        dfp - destination foundation pile
+
+        Keyword arguments:
+        c -- card in question
+        dfp -- destination foundation pile
         """
         return False
 
