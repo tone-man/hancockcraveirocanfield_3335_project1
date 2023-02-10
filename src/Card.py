272510046df587fd75a850f53d03b30b3b8594d0
William Hancock <williamhancock328@gmail.com>
2023-02-10 13:13:17 -0500

    Card now has a tostring method, view is now able to print first line

--- src/Card.py
+++ src/Card.py
@@ -9,3 +9,12 @@ class Card:
 
     def getSuit(self):
         return self.suit
+    
+    def toString(self):
+        num = self.getNumber()
+        suit = self.getSuit()
+        if num == None and suit == None:
+            return "---- \n" + "|   | \n" + "---- "
+        if num == '10':
+            return self.number + "-- \n" + self.suit + "  | \n" + "---- "
+        return self.number + "--- \n" + self.suit + "  | \n" + "---- "
--- src/View.py
+++ src/View.py
@@ -1,19 +1,55 @@
 #Take gameboard and print it to the screen
 from Board import Board
 from Card import Card
+import pandas as pd
 b = Board(4)
 cells = b.getFreeCells()
-cells[0] = Card("♥", 1)
-cells[1] = Card("♥", 2)
-cells[2] = Card("♥", 3)
-cells[3] = Card("♥", 4)
-def updateView(board):
-    #Print the board
-    print("Free Cells:")
-    for i in range(len(b.freeCells)):
-        print (fcellprint(b.freeCells[i]))
+cells[0] = Card("♥", '2')
+cells[1] = Card("♥", '6')
+cells[2] = Card("♥", '3')
+cells[3] = Card("♥", '4')
+found = b.getFoundations()
 
-def fcellprint(card):
-    print( card.getNumber(), card.getSuit())
+def updateView(board):
+
+
+    BoardStrs = []
+    for card in cells:
+        BoardStrs.append(card.toString())
+    for card in found:
+        BoardStrs.append(card.toString())
+    fcellprint(BoardStrs)
+
+
+
+    #Print the board
+    #print(b.freeCells[0].toString(), b.freeCells[1].toString(), b.freeCells[2].toString(), b.freeCells[3].toString())
+    
+    #for i in range(len(b.freeCells)):
+        #print (fcellprint(b.freeCells[i]))
+
+#https://stackoverflow.com/questions/43372078/how-to-print-multiline-strings-on-the-same-line-in-python
+def fcellprint(strings):
+    strings_by_column = [s.split('\n') for s in strings]
+
+    # Group the split strings by line
+    # In this example, all strings are the same, so for each line we
+    # will have three copies of the same string.
+    strings_by_line = zip(*strings_by_column)
+
+    # Work out how much space we will need for the longest line of
+    # each multiline string
+    max_length_by_column = [
+        max([len(s) for s in col_strings])
+        for col_strings in strings_by_column
+    ]
+
+    for parts in strings_by_line:
+        # Pad strings in each column so they are the same length
+        padded_strings = [
+            parts[i].ljust(max_length_by_column[i])
+            for i in range(len(parts))
+        ]
+        print(''.join(padded_strings))
 #https://stackoverflow.com/questions/39230209/how-to-print-2-lists-vertically-next-to-each-other
 updateView(b)
