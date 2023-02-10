69faa0a082a2ed38143bc2141712983c05753add
William Hancock <williamhancock328@gmail.com>
2023-02-10 13:13:08 -0500

    added toString to card, view printing top of board

--- src/Board.py
+++ src/Board.py
@@ -1,19 +1,20 @@
 #Object that represents the board for a FreeCell game
+from Card import Card
 class Board:
     def __init__(self, numFreeCells):
-        self.freeCells = [0 for x in range(numFreeCells)]
-        self.tableaus = [[]] * 8
-        self.foundations = [] * 4
+        self.freeCells = [Card(None,None) for x in range(numFreeCells)]
+        self.tableaus = [[Card(None,None)]] * 8
+        self.foundations = [Card(None,None)] * 4
 
         #insert Deck Here
 
         #Do Dealing here
         
     
-    def reset(self):
-        self.freeCells = []
-        self.tableaus = []
-        self.foundations = []
+    def reset(self, numFreeCells):
+        self.freeCells = [0 for x in range(numFreeCells)]
+        self.tableaus = [[0]] * 8
+        self.foundations = [0] * 4
 
     def getFreeCells(self):
         return self.freeCells
