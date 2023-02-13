18fb8098b8237a69c3d503099f84e918910894ac
Toni <craveiroa@merrimack.edu>
2023-02-13 13:32:09 -0500

    Two More IsValids and controller is done

--- src/GameController.py
+++ src/GameController.py
@@ -18,41 +18,44 @@ class GameController:
         st -- source tableau
         dt -- destination 
         """
-        t = m.getTableaus()
-        c = t[st].peek()
+        c = st.peek()
 
-        if(self.isValidMove(c, t[dt])):
+        if(self.isValidMove(c, dt)):
             c = st.pop
-            t[dt].push(c)
+            dt.push(c)
             self.updateView()
 
 
 
-    def moveCard(self, st, dfc) -> None:
+    def moveCard(self, st, fcIdx) -> None:
         """Moves a card from the tableau to a freecell.
 
         Keyword arguments:
         st -- source tableau
-        dfc -- destination freecell
+        dfcIdx -- destination freecell index
         """
+        fc = m.getFreeCells()
         c = st.peek()
 
-        if(self.isValidMove(c, dfc)):
+        if(self.isValidMove(c, dfcIdx)):
             c = st.pop()
-            m.getfreeCells = c
+            fc[dfcIdx] = c
             self.updateView()
 
-    def moveCard(self, sfc, dt) -> None:
+    def moveCard(self, sfcIdx, dt) -> None:
         """Moves a card from a freecell to a tableau
 
         Keyword arguments:
-        sfc -- source free cell
+        sfc -- source free cell index
         dt -- destination tableau
         """
-        c = sfc.peek
+        fc = m.getFreeCells()
+        c = fc[sfcIdx]
 
         if(self.isValidMove(c, dt)):
-            sfc.pop()
+            fc[sfcIdx] = None
+            dt.push(c)
+            self.updateView()
 
     def moveFoundation(self, t) -> None:
         """Moves a card from a tableau to a foundation pile
@@ -60,15 +63,31 @@ class GameController:
         Keyword arguments:
         t -- source tableau
         """
-        pass
+        f = m.getFoundations()
+        c = t.peek()
+        s = c.getSuit()
 
-    def moveFoundation(fc) -> None:
+        if(self.isValidMove(c, f[s])):
+            c = t.pop()
+            f[s] = c
+            self.updateView()
+
+    def moveFoundation(fcIdx) -> None:
         """Moves a card from a freecell to a foundation
 
         Keyword arguments:
-        fc -- source free cell
+        fc -- source free cell index
         """
-        pass
+        fc = m.getFreeCells()
+        f = m.getFoundations()
+
+        c = fc[fcIdx]
+        s = c.getSuit()
+
+        if(self.isValidMove(c, f[s])):
+            fc[idx] =  None
+            f = c
+            self.updateView()
 
     def isValidMove(self, c, dt) -> bool:
         """Checks that card placement is valid.
@@ -79,13 +98,18 @@ class GameController:
         """
         return False
     
-    def isValidMove(self, c, fc) -> bool:
+    def isValidMove(self, c, fcIdx) -> bool:
         """Checks that card placement is valid.
 
         Keyword arguments:
         c -- card in question
-        fc -- destination freecell
+        fc -- destination freecell index
         """
+        f = m.getFreeCells()
+
+        if(f[fcIdx] == None):
+            return True
+
         return False
 
     
@@ -96,10 +120,13 @@ class GameController:
         c -- card in question
         dfp -- destination foundation pile
         """
+        if(f[fcIdx] == None):
+            return True
+
         return False
 
     def updateView(self) -> None:
         """Updates the view the controller is connected to.
         In this case, it is the gameView.
         """
-        pass
+        v.updateView(m)
