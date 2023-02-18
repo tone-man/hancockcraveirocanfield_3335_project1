b5a09423b0f4789f4fb0c937f846e98f50ee82fd
William Hancock <williamhancock328@gmail.com>
2023-02-18 14:02:13 -0500

    ensured that the free cell is cleared when moving a card to a foundation

--- src/gameAgent.py
+++ src/gameAgent.py
@@ -160,7 +160,7 @@ class gameAgent:
                     copyC = copyFC[i]
                     copyF[copyC.getSuit()] = copyC
                     copyC = None
-
+                    copyFC[i] = None
                     node.addNext(Node(copyB, 1))
     
 
